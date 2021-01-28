a67430a5b8c6c8a42d783f24d931c1e7da0660f0
runner <runner@Mac-1611844647949.local>
2021-01-28 14:43:39 +0000

    release/0.4.0

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+import PackageDescription
+let package = Package(
+    name: "ZendeskSDKHTTPClient",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "ZendeskSDKHTTPClient",
+            targets: [
+                "ZendeskSDKHTTPClientTargets"
+            ]
+        )
+    ],
+    dependencies: [
+        .package(name: "ZendeskSDKLogger",
+                 url: "https://github.com/zendesk/sdk_logger_ios",
+                 from: "0.4.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ZendeskSDKHTTPClient",
+            path: "ZendeskSDKHTTPClient.xcframework"
+        ),
+        .target(name: "ZendeskSDKHTTPClientTargets",
+                dependencies: [
+                    .target(name: "ZendeskSDKHTTPClient"),
+                    .product(name: "ZendeskSDKLogger", package: "ZendeskSDKLogger")
+                ],
+                path: "Sources")
+    ]
+)
